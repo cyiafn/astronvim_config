d45a22ede3a30d8e67c3503c215d9f9bf393b785
yifan.chen1 <yifan.chen1@bytedance.com>
2024-12-16 12:32:38 +0700

    added bindings for yanking file path

--- lua/custom-bindings.lua
+++ lua/custom-bindings.lua
@@ -3,6 +3,24 @@ local map = vim.api.nvim_set_keymap
 -- general
 map("n", "<leader>w", ":w<CR>", { silent = false, desc = "[w]rite file" })
 map("n", "<leader>q", ":q<CR>", { silent = false, desc = "[q]uit buffer" })
+map(
+  "n",
+  "<leader>ypr",
+  ':let @+ = expand("%")<CR>',
+  { noremap = true, silent = true, desc = "[y]ank [p]ath [r]elative" }
+)
+map(
+  "n",
+  "<leader>ypa",
+  ':let @+ = expand("%:p")<CR>',
+  { noremap = true, silent = true, desc = "[y]ank [p]ath [a]bsolute" }
+)
+map(
+  "n",
+  "<leader>cr",
+  ':let @+ = expand("%") . ":" . line(".")<CR>',
+  { noremap = true, silent = true, desc = "[y]ank relative [p]ath with [l]ine number" }
+)
 
 -- buffer
 vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { desc = "Close current buffer" })
